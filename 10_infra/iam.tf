018879cac9347c5ae92952daaf0a609243c5331f
tomocareer <chatgptinjapan@gmail.com>
2024-09-06 19:20:56 +0900

    iamrole

--- 10_infra/iam.tf
+++ 10_infra/iam.tf
@@ -17,7 +17,7 @@ resource "aws_iam_role" "app_iam_role" {
     Name    = "${var.project}-${var.environment}-app-iam-role"
     Project = var.project
     Env     = var.environment
-    Message = "hellooffs"
+    Message = "This role is used by EC2 instances to access other AWS services"
   }
 }
 
